fb7ad25a22e7e3b1b1b8213063b484d6dc6a02de
wan <wan.com>
2019-07-02 10:39:20 +0800

    优化在线二维码生成

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+import io
 import flask
 import qrcode
 
-from datetime import timedelta
+# from datetime import timedelta
 
 
 app = flask.Flask(__name__)
 
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
+# app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
 
 @app.route('/')
 
@@ -22,20 +23,28 @@ def home():
     # return '<img src ="/static/qr.png" />'
     #第一次修改
 
-
-
-
     return flask.render_template('qr_tool.html')
 
-@app.route('/qr',methods=["POST"])
+# @app.route('/qr',methods=["POST"])
+@app.route('/qr')
+
 def qr():
-    data = flask.request.form.get("data")
-    img = qrcode.make(data)
+    # 第一步：获取要生成二维码的数据
+    #data = flask.request.form.get("data")
+    data = flask.request.args.get("data")
 
-    img.save(r"C:\Users\Administrator\Desktop\二维码生成器\static\qrc.png")
+    
+    # 第二步：生成二维码图片
+    img = qrcode.make(data) 
+    bi = io.BytesIO() # 创建一个BytesIO对象，用于二维码图像数据
 
-    return '<img src ="/static/qrc.png" />'
+    #img.save(r"C:\Users\Administrator\qr_tool\static\qrc.png") #保存到文件夹
 
+    img.save(bi,'png') # 通过调用img对象的save方法将二维码图像数据以PNG编码格式写入bi对象管理的内存空间
+    bi.seek(0) # 将bi对象内部的位置指针移动到图像数据的起始位置
+
+    # return '<img src ="/static/qrc.png" />'
+    return flask.send_file(bi, 'image/png')
 
 if __name__ == '__main__':
     app.run(debug=True)
